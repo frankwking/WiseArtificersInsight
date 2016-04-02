c735996380f4093b5447d6265e92f4c37ebe96b5
Frank King <frankwking@gmail.com>
2016-04-02 17:11:13 -0400

    added scales to graphing

--- public/index.js
+++ public/index.js
@@ -24,24 +24,35 @@ function fetchResultsJS(theForm,event) {
     success: function(data) {
       $.each(returnFloatArray, function(index, item) { document.getElementById(item).innerHTML = data[item].toFixed(2); });
       $.each(returnIntegerArray, function(index, item) { document.getElementById(item).innerHTML = data[item]; });
-      var hist = data.hist;
 
       var dataset = [];
-      $.each(hist, function(index, item) {dataset.push(item)});
+      var dataset2 = [];
+      $.each(data.hist, function(index, item) {dataset.push(item)});
+      $.each(data.hist, function(index, item) {dataset2.push([parseInt(index), item])});
 
       var svg = d3.select(".chart")
         .append("svg")
         .attr("width", chartWidth)
         .attr("height", chartHeight);
 
+      var xScale = d3.scale.linear()
+        .domain([d3.min(dataset2, function(d) {return d[0];}), d3.max(dataset2, function(d) {return d[0];})])
+        .range([0, chartWidth]);
+
+      var numBars = d3.max(dataset2, function(d) {return d[0];}) - d3.min(dataset2, function(d) {return d[0];});
+
+      var yScale = d3.scale.linear()
+        .domain([0, d3.max(dataset2, function(d) {return d[1];})])
+        .range([0, chartHeight]);
+
       svg.selectAll("rect")
-        .data(dataset)
+        .data(dataset2)
         .enter()
         .append("rect")
-        .attr("x", function(d,i){ return i * (chartWidth / dataset.length);})
-        .attr("y", function(d) {return chartHeight - d})
-        .attr("width", chartWidth / dataset.length - barPadding)
-        .attr("height",function(d) {return d})
+        .attr("x", function(d) {return xScale(d[0]);})
+        .attr("y", function(d) {return chartHeight - yScale(d[1]);})
+        .attr("width", chartWidth / numBars - barPadding)
+        .attr("height", function(d) {return yScale(d[1]);})
         .attr("fill","teal");
     }
   });
