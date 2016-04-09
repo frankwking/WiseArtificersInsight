0409cdcee70f0e6aa0bd72f0b28576f002c0ada1
Frank King <frankwking@gmail.com>
2016-04-09 00:36:37 -0400

    fixed rollPool name

---
+++ public/exaltedCraftingDieRoller.js
@@ -0,0 +1,9 @@
+function rollPool(initialPoolSize) {
+  var poolSize = initialPoolSize;
+
+  var i = 0;
+  var successes = 0;
+  var resultAry = [0,0,0,0,0,0,0,0,0,0];
+
+  console.log("This is a test.");
+}
