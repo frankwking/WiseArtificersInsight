f3119ff3175edbcb67931136558594f7dcda66a0
Frank King <frankwking@gmail.com>
2016-04-09 00:43:17 -0400

    added d10 roll function and called it to die in pool loop

--- public/exaltedCraftingDieRoller.js
+++ public/exaltedCraftingDieRoller.js
@@ -1,3 +1,9 @@
+// Return an interger between 0 and 9
+function rollD10() {
+  return Math.floor(Math.random() * (9));
+}
+
+// Roll a single craft pool of dice
 function rollPool(initialPoolSize) {
   var poolSize = initialPoolSize;
 
@@ -6,8 +12,8 @@ function rollPool(initialPoolSize) {
   var resultAry = [0,0,0,0,0,0,0,0,0,0];
 
   while ( i < poolSize) {
-    console.log(i);
     i += 1;
+    var die = rollD10();
   }
 
 }
