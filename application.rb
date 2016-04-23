48f18c0c59e933fadf2fbef3e5c5e083537c1593
Frank King <frankwking@gmail.com>
2016-04-23 16:06:01 -0400

    removed depreciated ruby commands to access the old ruby die roller

--- application.rb
+++ application.rb
@@ -1,15 +1,8 @@
 require 'sinatra'
 require "sinatra/json"
 require 'tilt/haml'
-require_relative 'ExaltedCraftingDieRoller'
-require_relative "ArrayStatsUtil"
 
 #Main Webpage Display
 get '/' do
-  ECDRInstance = ECDRApplication.new
   haml :index
-end
-
-get '/exaltedcraftingdieroller' do
-  json(ECDRInstance.resultsJson(params), encoder: :to_json, content_type: :js)
 end
